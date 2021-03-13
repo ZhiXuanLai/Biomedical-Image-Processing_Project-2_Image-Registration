fb3a8648c950a4c22858619a008443f60e733257
Tina @NTU <32099929+Tina-Tina@users.noreply.github.com>
2021-03-13 17:14:23 +0800

    Image Registration.

---
+++ BIP_Project2_Q2_ImageRegistration.m
@@ -0,0 +1,34 @@
+%Q2 correlation
+figure(1);
+translate= imtranslate(mri_a, [-45,-45]);
+% translate image by left=45, up=45
+subplot(2,2,1);
+image(translate);
+colormap(gray(256));
+axis('image') ;
+title('translate by (-45, 45)');
+correlation_1=sum(sum(mri_a.*translate))
+
+translate_1= imtranslate(mri_a, [-30,-30]);
+subplot(2,2,2);
+image(translate_1);
+colormap(gray(256));
+axis('image') ;
+title('translate by (-30, 30)');
+
+correlation_2=sum(sum(mri_a.*translate_1))
+
+translate_2= imtranslate(mri_a, [-15,-15]);
+subplot(2,2,3);
+image(translate_2);
+colormap(gray(256));
+axis('image') ;
+title('translate by (-15, 15)');
+correlation_3=sum(sum(mri_a.*translate_2))
+
+subplot(2,2,4);
+image(mri_a);
+colormap(gray(256));
+axis('image') ;
+title('original site');
+correlation_4=sum(sum(mri_a.*mri_a)
